9dc0bb1a96d8fdf02dcd0294eb37580f3f89ae43
Ion19 <engabdelrhman2016@gmail.com>
2019-09-19 06:37:59 +0200

    Updates

---
+++ precache-manifest.495157aa77fba6ab25f8e65b345e3130.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8556ee0c9847932e6138a2260c136a",
+    "url": "/arabic-localizer-analytical/index.html"
+  },
+  {
+    "revision": "01763cfb2f39ebc42049",
+    "url": "/arabic-localizer-analytical/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5f14be23814148e165c9",
+    "url": "/arabic-localizer-analytical/static/css/main.9ea9f25c.chunk.css"
+  },
+  {
+    "revision": "01763cfb2f39ebc42049",
+    "url": "/arabic-localizer-analytical/static/js/2.3f7ca359.chunk.js"
+  },
+  {
+    "revision": "5f14be23814148e165c9",
+    "url": "/arabic-localizer-analytical/static/js/main.47a79180.chunk.js"
+  },
+  {
+    "revision": "32cb5fdae03a5ce3f086",
+    "url": "/arabic-localizer-analytical/static/js/runtime~main.48c20d95.js"
+  },
+  {
+    "revision": "4f0022f25672c7f501c339cbf98d9117",
+    "url": "/arabic-localizer-analytical/static/media/fontawesome-webfont.4f0022f2.ttf"
+  },
+  {
+    "revision": "776d58f453c8fe5d6a89e9c31ee223ff",
+    "url": "/arabic-localizer-analytical/static/media/fontawesome-webfont.776d58f4.svg"
+  },
+  {
+    "revision": "90186830c9c50a0fed932494581761d9",
+    "url": "/arabic-localizer-analytical/static/media/fontawesome-webfont.90186830.eot"
+  },
+  {
+    "revision": "fdf491ce5ff5b2da02708cd0e9864719",
+    "url": "/arabic-localizer-analytical/static/media/fontawesome-webfont.fdf491ce.woff"
+  },
+  {
+    "revision": "8adf8a6690d769e93428406c26bd5256",
+    "url": "/arabic-localizer-analytical/static/media/profile.8adf8a66.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/arabic-localizer-analytical/precache-manifest.495157aa77fba6ab25f8e65b345e3130.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arabic-localizer-analytical/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
